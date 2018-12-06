60f010865a3138474414226a45e676648b923769
Lindsay Constantino <lindsayconstantino@lindsays-mbp.wireless.rit.edu>
2018-12-05 21:43:23 -0500

    able to make menus show and hide separately

--- fall2018sample/fall2018sample.js
+++ fall2018sample/fall2018sample.js
@@ -1,13 +1,13 @@
 /* Don't use <script> tags in a linked js file! */
 
 $(".menuitem").click(function () {
-    $(".submenu").slideToggle(400);
+    $(this).next(".submenu").slideToggle(400); //next gets menus to show and hide separately
 });
 
 let content = ("content1.txt"); // sets default verse element
 $("#choose-content").val(content); // changes menu option to default
 $("#content").load(content); // retrieves only default element
 $("#choose-content").change(function () {
-    verse = $(this).val();
-    $("#content").load(verse);
+    content = $(this).val();
+    $("#content").load(content);
 });
