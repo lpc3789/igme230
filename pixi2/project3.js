8723d70cf9469f2c841b83d8c5eb6fdb9f464b19
Lindsay Constantino <lindsayconstantino@lindsays-mbp.wireless.rit.edu>
2018-12-08 15:31:26 -0500

    added clothing, still working on drag and drop feature

--- pixi2/project3.js
+++ pixi2/project3.js
@@ -1,5 +1,6 @@
+//pixi.js renderer takes up full screen and is transparent so things will render on screen properly
 var app = new PIXI.Application(800, 600, {
-    backgroundColor: 0xffffff
+    transparent: true
 });
 
 app.renderer.view.style.position = "absolute";
@@ -9,16 +10,21 @@ app.renderer.resize(window.innerWidth, window.innerHeight);
 
 
 // create a texture from an image path
-var texture = PIXI.Texture.fromImage("shirtIcon1.png");
+var blueShirt = PIXI.Texture.fromImage("shirtIcon1.png");
+var pinkShirt = PIXI.Texture.fromImage("shirtIcon2.png");
+var purpleShirt = PIXI.Texture.fromImage("shirtIcon3.png");
+var redShirt = PIXI.Texture.fromImage("shirtIcon4.png");
 
 
+createBlueShirt(830, 275);
+createPinkShirt(830, 275);
+createPurpleShirt(830, 275);
+createRedShirt(830, 275);
 
-createClothing(500, 500);
+function createBlueShirt(x, y) {
 
-function createClothing(x, y) {
-
-    // create our little bunny friend..
-    var clothes = new PIXI.Sprite(texture);
+    // create shirt
+    var clothes = new PIXI.Sprite(blueShirt);
 
     // enable the clothes to be interactive
     clothes.interactive = true;
@@ -30,7 +36,103 @@ function createClothing(x, y) {
     clothes.anchor.set(0.5);
 
     // scale it to size
-    clothes.scale.set(.5);
+    clothes.scale.set(.45);
+
+    // setup events for mouse + touch using
+    // the pointer events
+    clothes
+        .on('pointerdown', onDragStart)
+        .on('pointerup', onDragEnd)
+        .on('pointerupoutside', onDragEnd)
+        .on('pointermove', onDragMove);
+
+    // move the sprite to its designated position
+    clothes.x = x;
+    clothes.y = y;
+
+    // add it to the stage
+    app.stage.addChild(clothes);
+}
+function createPinkShirt(x, y) {
+
+    // create shirt
+    var clothes = new PIXI.Sprite(pinkShirt);
+
+    // enable the clothes to be interactive
+    clothes.interactive = true;
+
+    // hand-cursor appears on rollover
+    clothes.buttonMode = true;
+
+    // center the clothes anchor point
+    clothes.anchor.set(0.5);
+
+    // scale it to size
+    clothes.scale.set(.45);
+
+    // setup events for mouse + touch using
+    // the pointer events
+    clothes
+        .on('pointerdown', onDragStart)
+        .on('pointerup', onDragEnd)
+        .on('pointerupoutside', onDragEnd)
+        .on('pointermove', onDragMove);
+
+    // move the sprite to its designated position
+    clothes.x = x;
+    clothes.y = y;
+
+    // add it to the stage
+    app.stage.addChild(clothes);
+}
+function createPurpleShirt(x, y) {
+
+    // create shirt
+    var clothes = new PIXI.Sprite(purpleShirt);
+
+    // enable the clothes to be interactive
+    clothes.interactive = true;
+
+    // hand-cursor appears on rollover
+    clothes.buttonMode = true;
+
+    // center the clothes anchor point
+    clothes.anchor.set(0.5);
+
+    // scale it to size
+    clothes.scale.set(.45);
+
+    // setup events for mouse + touch using
+    // the pointer events
+    clothes
+        .on('pointerdown', onDragStart)
+        .on('pointerup', onDragEnd)
+        .on('pointerupoutside', onDragEnd)
+        .on('pointermove', onDragMove);
+
+    // move the sprite to its designated position
+    clothes.x = x;
+    clothes.y = y;
+
+    // add it to the stage
+    app.stage.addChild(clothes);
+}
+function createRedShirt(x, y) {
+
+    // create shirt
+    var clothes = new PIXI.Sprite(redShirt);
+
+    // enable the clothes to be interactive
+    clothes.interactive = true;
+
+    // hand-cursor appears on rollover
+    clothes.buttonMode = true;
+
+    // center the clothes anchor point
+    clothes.anchor.set(0.5);
+
+    // scale it to size
+    clothes.scale.set(.45);
 
     // setup events for mouse + touch using
     // the pointer events
